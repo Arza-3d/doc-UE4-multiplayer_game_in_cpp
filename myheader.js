9fda91f887cab00e3edbd5322d729f2384116c07
Forest Guhareza <arza.3d@gmail.com>
2019-07-31 15:31:31 +0700

    update

---
+++ myheader.js
@@ -0,0 +1,32 @@
+$('header').html(`<h1>multiplayer in C++</h1>
+  <h4>make multiplayer in C++</h4>
+  <table>
+    <tr>
+      <td>game engine</td>
+      <td>Unreal Engine 4.22</td>
+    </tr>
+    <tr>
+      <td>project repo</td>
+      <td><a href="https://github.com/Arza-3d/UE4-StealthGame_note">github.com/Arza-3d/UE4-StealthGame_note</a></td>
+    </tr>
+  </table>
+
+
+  <p>This is just bunch of notes on training C++ in UE4. Project doc will be splitted into 2 projects</p>
+
+  <ol>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/index.html">setup</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/playersetup.html">player setup</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/gamemoderules.html">game mode rules</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/aiguards.html">AI guards</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/multiplayersupport.html">multiplayer support</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/playersetup2.html">player setup</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/weapon.html">weapon</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/weaponextra.html">weapon extra</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/hitpointanddeath.html">hitpoint and death</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/gamenetworking.html">game networking</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/basicai.html">basic AI</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/powerups.html">power ups</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/coopgamemodesetup.html">coop game mode setup</a></li>
+    <li><a href="https://raw.githack.com/Arza-3d/doc-UE4_multiplayer_game_in_cpp/master/advancedai.html">advanced AI</a></li>
+  </ol>`);
